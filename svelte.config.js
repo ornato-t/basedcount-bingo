94e61329e0c02a5caac241f6ce249a05b154abd1
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-14 16:59:31 +0200

    Fixing  netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,20 @@
-import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+export default {
 	preprocess: vitePreprocess(),
 
-	adapter: adapter({
-		// if true, will create a Netlify Edge Function rather
-		// than using standard Node-based functions
-		edge: false,
+	kit: {
+		// default options are shown
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
 
-		// if true, will split your app into multiple functions
-		// instead of creating a single one for the entire app.
-		// if `edge` is true, this option cannot be used
-		split: false
-	})
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
+	}
 };
-
-export default config;
