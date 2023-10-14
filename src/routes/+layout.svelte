eade48369004fb30da27a2e93137c026cb59289c
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-14 17:15:50 +0200

    Adding meta tags and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,30 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+	<!-- COMMON TAGS -->
+	<meta charset="utf-8">
+	<title>Based Count Bingo</title>
+	<!-- Search Engine -->
+	<meta name="description" content="Enter the fray of the Based Count Discord, where being terminally online is not a choice, but a challenge.">
+	<meta name="image" content="https://basedcount-bingo.netlify.app/favicon.png">
+	<!-- Schema.org for Google -->
+	<meta itemprop="name" content="Based Count Bingo">
+	<meta itemprop="description" content="Enter the fray of the Based Count Discord, where being terminally online is not a choice, but a challenge.">
+	<meta itemprop="image" content="https://basedcount-bingo.netlify.app/favicon.png">
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:title" content="Based Count Bingo">
+	<meta name="twitter:description" content="Enter the fray of the Based Count Discord, where being terminally online is not a choice, but a challenge.">
+	<meta name="twitter:image:src" content="https://basedcount-bingo.netlify.app/favicon.png">
+	<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+	<meta name="og:title" content="Based Count Bingo">
+	<meta name="og:description" content="Enter the fray of the Based Count Discord, where being terminally online is not a choice, but a challenge.">
+	<meta name="og:image" content="https://basedcount-bingo.netlify.app/favicon.png">
+	<meta name="og:url" content="https://basedcount-bingo.netlify.app/">
+	<meta name="og:site_name" content="Based Count Bingo">
+	<meta name="og:locale" content="en_GB">
+	<meta name="og:type" content="website">
+</svelte:head>
+
 <slot />
