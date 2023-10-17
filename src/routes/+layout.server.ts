5b2f8bdc32d9dae92c8c6b7d41d1e8099488cac4
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-18 01:55:54 +0200

    Adding victories to users array

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,8 +3,12 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async ({ cookies, locals }) => {
     const { sql } = locals;
     const users = sql`
-        SELECT name, admin, image, banner, discord_id
-        FROM discord_user      
+        SELECT name, admin, image, banner, COUNT(round_number) as victories
+        FROM discord_user
+        LEFT JOIN discord_user_wins_round win
+        ON discord_user.discord_id=win.discord_user_discord_id
+        GROUP BY name, admin, image, banner
+        ORDER BY victories DESC      
     `;
 
     return {
