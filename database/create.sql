2850a812357cf6c44b100dd02f6ccaadb20414c4
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-20 16:18:01 +0200

    Changing round number from text to integer

--- database/create.sql
+++ database/create.sql
@@ -17,27 +17,27 @@ CREATE TABLE box (
     FOREIGN KEY (creator_discord_id) REFERENCES discord_user(discord_id)
 );
 CREATE TABLE round (
-    number TEXT NOT NULL,
+    number SERIAL NOT NULL,
     PRIMARY KEY (number)
 );
 CREATE TABLE card (
     owner_discord_id TEXT NOT NULL,
-    round_number TEXT NOT NULL,
+    round_number INTEGER NOT NULL,
     PRIMARY KEY (owner_discord_id, round_number),
     FOREIGN KEY (owner_discord_id) REFERENCES discord_user(discord_id),
     FOREIGN KEY (round_number) REFERENCES round(number)
 );
 CREATE TABLE box_in_card (
     box_id INTEGER NOT NULL,
-    card_user_discord_id TEXT NOT NULL,
-    card_round_number TEXT NOT NULL,
-    PRIMARY KEY (box_id, card_user_discord_id, card_round_number),
+    card_owner_discord_id TEXT NOT NULL,
+    card_round_number INTEGER NOT NULL,
+    PRIMARY KEY (box_id, card_owner_discord_id, card_round_number),
     FOREIGN KEY (box_id) REFERENCES box(id),
-    FOREIGN KEY (card_user_discord_id, card_round_number) REFERENCES card(owner_discord_id, round_number)
+    FOREIGN KEY (card_owner_discord_id, card_round_number) REFERENCES card(owner_discord_id, round_number)
 );
 CREATE TABLE discord_user_wins_round (
     discord_user_discord_id TEXT NOT NULL,
-    round_number TEXT NOT NULL,
+    round_number INTEGER NOT NULL,
     PRIMARY KEY (discord_user_discord_id, round_number),
     FOREIGN KEY (discord_user_discord_id) REFERENCES discord_user(discord_id),
     FOREIGN KEY (round_number) REFERENCES round(number)
