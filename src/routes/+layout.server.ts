e79247ec50b48b01f23a620ac409d71afef422b2
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-22 15:09:27 +0200

    Add promise waterfall in initial load: slower load but less resource usage
    Reduce concurrent connection to 1 per user, otpimizes 5 concurrent connections from free plan

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,38 +5,36 @@ export const load: LayoutServerLoad = async ({ cookies, locals }) => {
     const { sql } = locals;
     const token = cookies.get('bingo-token');
 
-    const [users, currentUserDb] = await Promise.all([
-        sql`
-            SELECT discord_id, name, admin, image, banner, COUNT(round_number) as victories
-            FROM discord_user
-            LEFT JOIN discord_user_wins_round win
-            ON discord_user.discord_id=win.discord_user_discord_id
-            GROUP BY name, admin, image, banner, discord_id
-            ORDER BY victories DESC      
-        `,
-        sql`
-            SELECT name, admin, image, banner, COUNT(round_number) as victories
-            FROM discord_user
-            LEFT JOIN discord_user_wins_round win
-            ON discord_user.discord_id=win.discord_user_discord_id
-            WHERE token=${token ?? null}
-            GROUP BY name, admin, image, banner
-        `
-    ]);
+    //While Promise.all() would be better for performances, this promise waterfall helps minimizing concurrent connections to the DB
+    const users = await sql`
+        SELECT discord_id, name, admin, image, banner, COUNT(round_number) as victories
+        FROM discord_user
+        LEFT JOIN discord_user_wins_round win
+        ON discord_user.discord_id=win.discord_user_discord_id
+        GROUP BY name, admin, image, banner, discord_id
+        ORDER BY victories DESC      
+    ` as User[];
 
-    let currentUser: User | null;
+    const currentUserDb = await sql`
+        SELECT name, admin, image, banner, COUNT(round_number) as victories
+        FROM discord_user
+        LEFT JOIN discord_user_wins_round win
+        ON discord_user.discord_id=win.discord_user_discord_id
+        WHERE token=${token ?? null}
+        GROUP BY name, admin, image, banner  
+    ` as CurrentUserDb[];
+
+    let currentUser: CurrentUser | null;
     if (currentUserDb.length === 0) {
         currentUser = null
     } else {
         currentUser = {
             admin: currentUserDb[0].admin,
             banner: currentUserDb[0].banner,
-            discord_id: currentUserDb[0].discord_id,
             image: currentUserDb[0].image,
             name: currentUserDb[0].name,
-            token: currentUserDb[0].token,
             victories: Number.parseInt(currentUserDb[0].victories)
-        } satisfies User;
+        };
     }
 
     return {
@@ -45,3 +43,19 @@ export const load: LayoutServerLoad = async ({ cookies, locals }) => {
         currentUser
     };
 };
+
+interface CurrentUserDb {
+    name: string,
+    admin: boolean,
+    image: string,
+    banner: string,
+    victories: string
+}
+
+interface CurrentUser {
+    name: string,
+    admin: boolean,
+    image: string,
+    banner: string,
+    victories: number
+}
