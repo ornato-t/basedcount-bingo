3d58205b00a5e598ac005b990098745502bbb339
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-23 00:22:42 +0200

    Temporarily sending messages in test channel

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -2,4 +2,5 @@ export const serverId = '826405737093136434';
 export const bingoPlayerRole = '1146738386544111626';
 export const bingoMasterRole = '1162879967705694318';
 export const adminRole = '928983928289771560';
-export const bingoChannelId = '1147008618894471188';
+// export const bingoChannelId = '1147008618894471188'; //Bingo channel
+export const bingoChannelId = '1137060282309562415';    //Playroom channel
