d6787ab3b3f6529c3cec43e49ebf15b846af8f5e
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-22 18:12:51 +0200

    Adding Discord url modal, accepting url in action

--- src/routes/play/+page.server.ts
+++ src/routes/play/+page.server.ts
@@ -38,14 +38,17 @@ export const actions = {
         const { sql } = locals;
 
         const formData = await request.formData();
+        console.log(formData)
         const token = formData.get('token');
         const boxId = formData.get('box');
+        const url = formData.get('url');
         const valueField = formData.get('value') ?? { toString: () => '' };
-        const value = valueField.toString() === 'on' ? true : false;
+        const value = valueField.toString() === 'true' ? true : false;
 
         if (token === null || boxId === null) return;
         if(Number.isNaN(Number.parseInt(boxId.toString()))) return; //KEKW can't be unchecked
-
+        if (url === null && value === true) return; //Ticking a box requires a URL to be specified
+        //TODO: do something with url
         if (value) {
             await sql`
                 INSERT INTO checks (discord_user_discord_id, box_id, card_owner_discord_id, card_round_number, time)
--- src/routes/play/bingoBoard.svelte
+++ src/routes/play/bingoBoard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { serverId } from '$lib/discord';
 	import { fit, parent_style } from '@leveluptuts/svelte-fit';
 	import type { BoxCheckable as Box } from './+page.server';
 
@@ -9,8 +10,18 @@
 
 	const getImgUrl = (card: Box) => card.text.substring(card.text.lastIndexOf(':') + 1); //Extracts "/sample.png" from "image:/sample.png"
 
+	let selectedBox: Box = { about_discord_id: null, checked: false, creator_discord_id: '', id: NaN, text: '' };
+
 	function boxClicked(box: Box) {
-		document.querySelector(`input[name='box'][value='${box.id}']`)!.closest('form')!.submit();
+		if (box.checked) {
+			//If the box is being checked ask for confirmation
+			selectedBox = box;
+			// @ts-ignore
+			clickBox.showModal();
+		} else {
+			//If the box is being unchecked just submit the form
+			document.querySelector(`input[name='box'][value='${box.id}']`)!.closest('form')!.submit();
+		}
 	}
 </script>
 
@@ -18,22 +29,59 @@
 	{#each cards as box}
 		<form method="post" action="?/check" class="w-16 h-16 sm:w-28 sm:h-28 border border-secondary cursor-pointer relative">
 			<label>
-				<input name="value" type="checkbox" class="opacity-0 absolute inset-0 w-full h-full" bind:checked={box.checked} on:change={() => boxClicked(box)}/>
+				<input name="value" type="checkbox" class="opacity-0 absolute inset-0 w-full h-full" bind:checked={box.checked} on:change={() => boxClicked(box)} />
 				<div style="position: relative; {parent_style}">
 					{#if regexImage.test(box.text)}
 						<img src={getImgUrl(box)} alt={getImgUrl(box)} />
 					{:else}
 						<h1 use:fit>{box.text}</h1>
 					{/if}
+					{#if box.checked}
+						<svg class="absolute top-0 left-0 w-full h-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+						</svg>
+					{/if}
 				</div>
-				{#if box.checked}
-					<svg class="absolute top-0 left-0 w-full h-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-					</svg>
-				{/if}
 			</label>
 			<input name="token" type="hidden" value={token} />
 			<input name="box" type="hidden" value={box.id} />
 		</form>
 	{/each}
 </div>
+
+<dialog id="clickBox" class="modal">
+	<div class="modal-box max-w-md">
+		<h3 class="font-bold text-lg">Confirm your action</h3>
+
+		<p class="pt-4">You are trying to tick the following box:</p>
+		<p class="pl-2 py-4 font-mono">{selectedBox.text}</p>
+		<p class="">Enter a link to the message that triggered it.</p>
+		<p class="py-1.5 text-sm italic">Hint: right click the message and select "Copy Message Link"</p>
+
+		<form method="post" action="?/check" class="modal-action flex flex-col">
+			<input 
+				type="url" name="url" id="url" title="the URL must be a Discord message link" required
+				placeholder="https://discord.com" pattern="https:\/\/discord\.com\/channels\/{serverId}\/\d+\/\d+"  
+				class="input input-bordered input-primary w-full"
+			/>
+			<input name="token" type="hidden" value={token} />
+			<input name="box" type="hidden" value={selectedBox.id} />
+			<input name="value" type="hidden" value={selectedBox.checked} />
+
+			<div class="mt-8 modal-action">
+				<!-- svelte-ignore missing-declaration -->
+				<button
+					class="btn btn-secondary btn-outline"
+					type="button"
+					on:click={() => {
+						// @ts-ignore
+						clickBox.close();
+					}}
+				>
+					Close
+				</button>
+				<button type="submit" class="btn btn-secondary"> Confirm </button>
+			</div>
+		</form>
+	</div>
+</dialog>
