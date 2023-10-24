dda04cdbc9700b2f4e63eb04bfc4d853a11acac5
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-24 21:33:02 +0200

    Testing automatic database connection kill

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,11 @@ import postgres from 'postgres'
 import { PG_URI } from "$env/static/private";
 import type { Handle } from '@sveltejs/kit';
 
-const sql = postgres(PG_URI);
+const sql = postgres(PG_URI, {
+    idle_timeout: 5,
+    max_lifetime: 30,
+
+});
 
 export const handle: Handle = (async ({ event, resolve }) => {
     event.locals.sql = sql;
