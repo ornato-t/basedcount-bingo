6e195870cb0f47ad90dad638c87f1f1a79b5ed68
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-26 23:32:03 +0200

    Making log always visible

--- src/routes/play/log.svelte
+++ src/routes/play/log.svelte
@@ -19,7 +19,10 @@
 	}
 </script>
 
-<aside class="h-96 lg:h-[35rem] carousel carousel-vertical carousel-end rounded-lg bg-neutral p-1.5">
+<aside class="h-96 lg:h-[35rem] w-full carousel carousel-vertical carousel-end rounded-lg bg-neutral p-1.5">
+	{#if entries.length === 0}
+		<div class="divider">No boxes have been ticked</div>
+	{/if}
 	{#each entries as log, i}
 		{#if newDay(log, i)}
 			<div class="divider">{log.time.toLocaleDateString()}</div>
@@ -39,9 +42,7 @@
 					{log.text}
 				</div>
 			{:else}
-				<div class="chat-bubble chat-bubble-accent uppercase">
-					bingo!
-				</div>
+				<div class="chat-bubble chat-bubble-accent uppercase">bingo!</div>
 			{/if}
 		</div>
 	{/each}
