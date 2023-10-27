955500ca3f6f11859f10a7ed5fd72134a0387ed6
Tommaso Ornato <tommy.ornato@gmail.com>
2023-10-27 18:43:11 +0200

    Improving image path generation

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,2 +1,2 @@
 export const regexImage = /image:\/\S+\.(jpg|jpeg|png|gif|webp|svg)/; //Matches image:/
-export const getImgUrl = (card: { text: string }) => card.text.substring(card.text.lastIndexOf(':') + 1); //Extracts "/sample.png" from "image:/sample.png"
+export const getImgUrl = (card: { text: string }) => card.text.substring(card.text.indexOf(':') + 2); //Extracts "/sample.png" from "image:/sample.png"
