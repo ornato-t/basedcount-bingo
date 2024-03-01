fc46d9d6dc013f50b7bb3ee3379b6159307bf970
Tommaso Ornato <tommy.ornato@gmail.com>
2024-03-01 15:26:47 +0100

    Better IP logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,12 +5,11 @@ import type { Handle } from '@sveltejs/kit';
 const sql = postgres(PG_URI, {
     idle_timeout: 5,
     max_lifetime: 10,    //Play page refreshes every 10 seconds
-
 });
 
 export const handle: Handle = (async ({ event, resolve }) => {
-    const ip = [event.request.headers.get('x-forwarded-for'), event.request.headers.get('x-real-ip')];
-    console.log(`Request from IP: [${ip}]`);
+    const ip = event.request.headers.get('x-forwarded-for');
+    console.log(`Request from IP: ${ip}`);
     
     event.locals.sql = sql;
 
