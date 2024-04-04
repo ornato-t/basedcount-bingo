09eafa515bb75729047086834bd39cbe37855ab2
Tommaso Ornato <tommy.ornato@gmail.com>
2024-04-04 16:54:25 +0200

    Explicit path for cookie deletion on logout (SvelteKit 2.0 compatibility)

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -2,6 +2,6 @@ import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ cookies }) => {
-    cookies.delete('bingo-token');
+    cookies.delete('bingo-token', { path: '/' });
     throw redirect(308, '/');
 };
